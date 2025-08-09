a2cb215ee53516b3b41c698c2341c2a7022e9d77
alpitg <alpitgajbhiye@gmail.com>
2025-08-10 03:04:38 +0530

    invoice change

--- app/schemas/orders/invoice.py
+++ app/schemas/orders/invoice.py
@@ -1,14 +1,16 @@
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 from pydantic import BaseModel
 
-# class PartyDetails(BaseModel):
+# class PartyDetails[BaseModel]:
 #     name: str
 #     detail: str
 #     phone: str
 
 class InvoiceIn(BaseModel):
-    billDate: str
-    billFrom: Dict[str, Any] # Optional[PartyDetails] = None
-    billTo: Dict[str, Any] # Optional[PartyDetails] = None
-    paymentMode: str
-    paymentStatus: str = "pending"
+    generateInvoice: bool = False
+    billDate: Optional[str]
+    billFrom: Optional[Dict[str, Any]] # Optional[PartyDetails] = None
+    billTo: Optional[Dict[str, Any]] # Optional[PartyDetails] = None
+    paymentMode: Optional[str]
+    paymentStatus: Optional[str] = "pending"
+    
