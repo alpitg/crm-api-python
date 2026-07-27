10f1567c10f645f34fc0415c3a63fd37ea38be32
alpitg <alpitgajbhiye@gmail.com>
2026-07-27 23:47:50 +0530

    swagger

--- core/routes.py
+++ core/routes.py
@@ -20,40 +20,40 @@ def setup_router(app: FastAPI) -> None:
     """
     Register all routes with the FastAPI application.
     """
-    app.include_router(ping.router, prefix="/api/ping", tags=["Public"])
+    app.include_router(ping.router, prefix="/ping", tags=["Public"])
 
     #region Public
     from app.modules.products.public.product_public_route import public_router
-    app.include_router(public_router, prefix="/api/store", tags=["Public"])
+    app.include_router(public_router, prefix="/store", tags=["Public"])
     #endregion
 
     #region Meal Planner
     from app.modules.meal_planner import meal_planner_routes
-    app.include_router(meal_planner_routes.router, prefix="/api/meal-planner", tags=["Meal Planner"])
+    app.include_router(meal_planner_routes.router, prefix="/meal-planner", tags=["Meal Planner"])
 
     #region Administration
-    app.include_router(auth_routes.router, prefix="/api/auth", tags=["Auth"])
-    app.include_router(organisation_units_routes.router, prefix="/api/organization-units", tags=["Organization Units"])
-    app.include_router(role_routes.router, prefix="/api/roles", tags=["Roles"])
-    app.include_router(role_permissions_routes.router, prefix="/api/roles-permissions", tags=["Roles-permissions"])
-    app.include_router(user_routes.router, prefix="/api/users", tags=["Users"])
+    app.include_router(auth_routes.router, prefix="/auth", tags=["Auth"])
+    app.include_router(organisation_units_routes.router, prefix="/organization-units", tags=["Organization Units"])
+    app.include_router(role_routes.router, prefix="/roles", tags=["Roles"])
+    app.include_router(role_permissions_routes.router, prefix="/roles-permissions", tags=["Roles-permissions"])
+    app.include_router(user_routes.router, prefix="/users", tags=["Users"])
     #endregion
 
     #region Catalog
-    app.include_router(products_route.router, prefix="/api/products", tags=["Products"])
-    app.include_router(customer_route.router, prefix="/api/customer", tags=["Customer"])
-    app.include_router(orders_route.router, prefix="/api/orders", tags=["Orders"])
+    app.include_router(products_route.router, prefix="/products", tags=["Products"])
+    app.include_router(customer_route.router, prefix="/customer", tags=["Customer"])
+    app.include_router(orders_route.router, prefix="/orders", tags=["Orders"])
     #endregion
 
     #region Invoice
-    app.include_router(invoice_routes.router, prefix="/api/invoices", tags=["Invoices"])
+    app.include_router(invoice_routes.router, prefix="/invoices", tags=["Invoices"])
     #endregion
 
 
     # region master routes
-    app.include_router(frame_type_routes.router, prefix="/api/master/frame_types", tags=["Master - Frame Types"])
-    app.include_router(glass_type_routes.router, prefix="/api/master/glass_types", tags=["Master - Glass Types"])
-    app.include_router(misc_charges_routes.router, prefix="/api/master/misc_charges", tags=["Master - Misc Charges"])
-    app.include_router(mount_type_routes.router, prefix="/api/master/mount_types", tags=["Master - Mount Types"])
-    app.include_router(order_status_routes.router, prefix="/api/master/order_status", tags=["Master - Order Status"])
+    app.include_router(frame_type_routes.router, prefix="/master/frame_types", tags=["Master - Frame Types"])
+    app.include_router(glass_type_routes.router, prefix="/master/glass_types", tags=["Master - Glass Types"])
+    app.include_router(misc_charges_routes.router, prefix="/master/misc_charges", tags=["Master - Misc Charges"])
+    app.include_router(mount_type_routes.router, prefix="/master/mount_types", tags=["Master - Mount Types"])
+    app.include_router(order_status_routes.router, prefix="/master/order_status", tags=["Master - Order Status"])
     # endregion
