2128011007cac620ea944daa9abeae146509c898
alpitg <alpitgajbhiye@gmail.com>
2026-07-28 02:41:13 +0530

    artisanstudios_db

--- app/db/mongo.py
+++ app/db/mongo.py
@@ -1,6 +1,17 @@
-from motor.motor_asyncio import AsyncIOMotorClient
 import os
+import certifi
+from motor.motor_asyncio import AsyncIOMotorClient
 
 MONGO_URL = os.getenv("MONGO_URL", "mongodb://localhost:27017")
-client = AsyncIOMotorClient(MONGO_URL)
-db = client["posterica_db"]
+
+print("MongoDB URL:", MONGO_URL)
+
+if MONGO_URL.startswith("mongodb+srv://"):
+    client = AsyncIOMotorClient(
+        MONGO_URL,
+        tlsCAFile=certifi.where()
+    )
+else:
+    client = AsyncIOMotorClient(MONGO_URL)
+
+db = client["artisanstudios_db"]
